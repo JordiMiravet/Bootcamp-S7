417030b13d6774ef0ce4a42ddae56abb38dd3756
Jordi Miravet <jordimiravet.dev@gmail.com>
2025-12-11 19:09:56 +0100

    feat(auth/register, environments, gitignore): add register styles, environment config to protect Firebase key, and update .gitignore to exclude it

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,12 +14,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(), 
     provideFirebaseApp(() => initializeApp({ 
-      projectId: "s7-starwars-app", 
-      appId: "1:853550212398:web:288bf72f65e8248a77c6bf", 
-      storageBucket: "s7-starwars-app.firebasestorage.app", 
-      apiKey: "AIzaSyBHqTYiZfWA-pgwD9DdQ8nGGmy9MGViAfE", 
-      authDomain: "s7-starwars-app.firebaseapp.com", 
-      messagingSenderId: "853550212398" 
+      projectId:environment.projectFirebaseId ,
+      appId:environment.appFirebaseId, 
+      storageBucket:environment.storageBucketFirebase, 
+      apiKey:environment.apiKeyFirebase, 
+      authDomain:environment.authDomainFirebase, 
+      messagingSenderId:environment.messagingSenderFirebaseId 
      })), 
       provideAuth(() => getAuth())
   ]
